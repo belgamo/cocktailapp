7c1e87a568e4773235091e0226b3c9a791d1cc71
Gabriel Belgamo <ggbelgamo@outlook.com>
2021-01-04 18:21:55 -0300

    chore(ios): add react-native-paper

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import App from './App';
 import { name as appName } from './app.json';
+
+Icon.loadFont();
 
 export default function Main() {
   return (
