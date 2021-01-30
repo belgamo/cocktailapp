cdeb0c0456643c5b4a2da7eb10a78bf5c3db8bbe
Gabriel Belgamo <ggbelgamo@outlook.com>
2021-01-30 11:18:41 -0300

    rename drink variable

--- src/components/Drink/index.js
+++ src/components/Drink/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { List, Avatar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
-const Drink = ({ drink }) => {
+const Drink = ({ drinkObject }) => {
   const navigation = useNavigation();
 
   return (
     <List.Item
       style={{ backgroundColor: '#fff' }}
-      title={drink.strDrink}
+      title={drinkObject.strDrink}
       left={(props) => (
         <Avatar.Image
           {...props}
           size={50}
-          source={{ uri: drink.strDrinkThumb }}
+          source={{ uri: drinkObject.strDrinkThumb }}
         />
       )}
       right={(props) => <List.Icon {...props} icon="chevron-right" />}
       onPress={() =>
         navigation.navigate('DrinkDetails', {
-          id: drink.idDrink,
-          name: drink.strDrink,
+          id: drinkObject.idDrink,
+          name: drinkObject.strDrink,
         })
       }
     />
